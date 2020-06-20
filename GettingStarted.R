82c41877eedb37545f81133d16fb645b6e866a6d
adivea <adela@fedarch.org>
2020-06-20 15:20:29 +0200

    intialize website

---
+++ GettingStarted.R
@@ -0,0 +1,24 @@
+# Packages
+
+install.packages("blogdown")
+library(blogdown)
+
+
+# install theme package and check version
+blogdown::install_hugo()
+blogdown::hugo_version()
+
+# default theme
+blogdown::new_site()
+
+# change stuff
+
+# navigate to AddInns and press 'Serve Site'
+
+# different theme (wait on this)
+# blogdown::new_site(theme = "themes/hugo-future-imperfect-skin", 
+#                    theme_example = TRUE)
+# 
+# # To see the current Hugo themes go to https://themes.gohugo.io/.
+# dir.create("themes")
+# dir("themes")
